fa1bc30ef0c9497f9ef6e7d186f79c1d8571cd1d
EstherIdabor <idaboresther@gmail.com>
2022-10-08 02:42:55 +0100

    made few changes

--- bookmark-landing-page-master/main.js
+++ bookmark-landing-page-master/main.js
@@ -1,3 +1,33 @@
+// creating a tabbed component
+const headerContainer = document.querySelector('.tabbed-heading')
+const featureHeaders = document.querySelectorAll('.feature-header')
+const bodyContainers = document.querySelectorAll('.tabbed-item')
+const barTracker = document.querySelector('.active-tab')
+
+
+headerContainer.addEventListener('click', (e) => {
+    // console.log(e.target)
+    if(e.target.dataset.num){
+        console.log(e.target.dataset.num)
+        const targetNum = e.target.dataset.num
+        
+        featureHeaders.forEach(featureHeader => {
+            featureHeader.classList.remove('active-feature')
+        })
+        headerContainer.querySelector(`.header-${targetNum}`).classList.add('active-feature')
+
+        bodyContainers.forEach(bodyContainer => {
+            bodyContainer.classList.remove('active-body')
+        })
+        document.querySelector(`.feature-body-${targetNum}`).classList.add('active-body')
+
+        barTracker.style.transform = `translateX(${targetNum * 100}% )`
+    }
+})
+
+
+
+// Creating an accordion
 const accordionItems = document.querySelectorAll('.accordion-question')
 
 accordionItems.forEach(accordionItem => {
